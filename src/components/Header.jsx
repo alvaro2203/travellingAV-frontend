76b5960e18f9aeede8f48618be8ded23bcd618de
Álvaro Aparicio <alvaro.apariciofdz@gmail.com>
2021-11-12 21:36:53 +0100

    modify the header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Flex, Spacer, Box, IconButton, useColorMode, Text } from "@chakra-ui/react"
+import { Flex, Spacer, Box, IconButton, useColorMode, Text, Divider} from "@chakra-ui/react"
 import Fade from 'react-reveal/Fade';
 
 //icons
@@ -12,38 +12,42 @@ export default function Header() {
   const { toggleColorMode } = useColorMode()
 
   return (
-    <Flex p="6">
-      <Box>
-        <Link to="/">
-          <Text fontSize="2xl">
-            <Fade left cascade>
-              {appName}
-            </Fade>
-          </Text>
-        </Link>
-      </Box>
-      <Spacer />
-      <Box mr="5">
-        <IconButton 
-          aria-label="cambiar tema de luz"
-          variant="solid"
-          colorScheme="blue"
-          size="md" 
-          onClick={toggleColorMode} 
-          icon={<FaRegLightbulb />}>
-        </IconButton>
-      </Box>
-      <Box>
-        <Link to="/login">
-          <IconButton
-            aria-label="Iniciar sesión"
+    <div>
+      <Flex p="10">
+        <Box>
+          <Link to="/">
+            <Text fontSize="2xl" color="brand.400">
+              <Fade left cascade>
+                {appName}
+              </Fade>
+            </Text>
+          </Link>
+        </Box>
+
+        <Spacer />
+
+        <Box mr="5">
+          <IconButton 
+            aria-label="cambiar tema de luz"
             variant="solid"
-            colorScheme="blue"
-            size="md"
-            icon={<MdLogin />}>
+            colorScheme="brand"
+            size="md" 
+            onClick={toggleColorMode} 
+            icon={<FaRegLightbulb />}>
           </IconButton>
-        </Link>
-      </Box>
-    </Flex>
+        </Box>
+        <Box>
+          <Link to="/login">
+            <IconButton
+              aria-label="Iniciar sesión"
+              variant="solid"
+              colorScheme="brand"
+              size="md"
+              icon={<MdLogin />}>
+            </IconButton>
+          </Link>
+        </Box>
+      </Flex>
+    </div>
   );
 }
